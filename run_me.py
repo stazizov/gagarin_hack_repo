cb27faf273573a47a2c3eb1017fd8de9779ca8cf
Said Azizov <69383296+stazizov@users.noreply.github.com>
2024-04-14 03:52:17 +0300

    mypy fix

--- run_me.py
+++ run_me.py
@@ -7,14 +7,14 @@ PATH_TO_TEST_DATA = pathlib.Path("data") / "test_texts.json"
 PATH_TO_OUTPUT_DATA = pathlib.Path("results") / "output_scores.json"
 
 
-def load_data(path: pathlib.PosixPath = PATH_TO_TEST_DATA) -> tp.List[str]:
+def load_data(path: pathlib.Path = PATH_TO_TEST_DATA) -> tp.List[str]:
     with open(path, "r", encoding="utf-8") as f:
         data = json.load(f)
 
     return data
 
 
-def save_data(data, path: pathlib.PosixPath = PATH_TO_OUTPUT_DATA):
+def save_data(data, path: pathlib.Path = PATH_TO_OUTPUT_DATA):
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=1, ensure_ascii=False)
 
